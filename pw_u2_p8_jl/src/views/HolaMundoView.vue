38b991a6fb74134b5ade2a28e8aa7c6aa3be1da4
Joel Red <joshua-lunag@hotmail.com>
2025-05-12 21:56:14 -0500

    Taller 12: Hola Mundo con Vue

---
+++ pw_u2_p8_jl/src/views/HolaMundoView.vue
@@ -0,0 +1,16 @@
+<template>
+  <HolaMundo></HolaMundo>
+</template>
+
+
+<script>
+import HolaMundo from "../components/HolaMundo.vue";
+export default {
+  components: {
+    HolaMundo,
+  },
+};
+</script>
+
+<style>
+</style>
